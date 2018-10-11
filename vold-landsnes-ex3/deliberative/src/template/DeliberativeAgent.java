0b5666d272442c1646f439482fe51b4539fc4998
martvo <martvo@stud.ntnu.no>
2018-10-11 13:55:00 +0200

    set up our deliberative agents metodes

---
+++ vold-landsnes-ex3/deliberative/src/template/DeliberativeAgent.java
@@ -0,0 +1,31 @@
+package template;
+
+import logist.agent.Agent;
+import logist.behavior.DeliberativeBehavior;
+import logist.plan.Plan;
+import logist.simulation.Vehicle;
+import logist.task.TaskDistribution;
+import logist.task.TaskSet;
+import logist.topology.Topology;
+
+public class DeliberativeAgent implements DeliberativeBehavior {
+
+	@Override
+	public void setup(Topology topology, TaskDistribution distribution, Agent agent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Plan plan(Vehicle vehicle, TaskSet tasks) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void planCancelled(TaskSet carriedTasks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
