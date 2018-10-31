e8986fb04dd50609a48f828eb66c5ba35951cdb8
martvo <martvo@stud.ntnu.no>
2018-10-31 11:42:58 +0100

    add centralized agent

---
+++ vold-landsnes-ex4/centralized/src/template/CentralizedAgent.java
@@ -0,0 +1,27 @@
+package template;
+
+import java.util.List;
+
+import logist.agent.Agent;
+import logist.behavior.CentralizedBehavior;
+import logist.plan.Plan;
+import logist.simulation.Vehicle;
+import logist.task.TaskDistribution;
+import logist.task.TaskSet;
+import logist.topology.Topology;
+
+public class CentralizedAgent implements CentralizedBehavior {
+
+	@Override
+	public void setup(Topology topology, TaskDistribution distribution, Agent agent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
