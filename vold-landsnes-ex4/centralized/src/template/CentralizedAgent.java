3edfdcab808f322005c0533544c359ba0fd82f9f
martvo <martvo@stud.ntnu.no>
2018-11-08 15:29:58 +0100

    100% Done

--- vold-landsnes-ex4/centralized/src/template/CentralizedAgent.java
+++ vold-landsnes-ex4/centralized/src/template/CentralizedAgent.java
@@ -93,7 +93,7 @@ public class CentralizedAgent implements CentralizedBehavior {
 			}
 
 			counter += 1;
-			if (System.currentTimeMillis() - time_start > timeout_plan) {
+			if (System.currentTimeMillis() - time_start > timeout_plan-1000) {
 				break;
 			}
 		}
