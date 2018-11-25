2240b8143cf44f9de435d26c26868cf51b41c361
martvo <martvo@stud.ntnu.no>
2018-11-25 17:50:30 +0100

    done?

--- vold-landsnes-ex5/auction/src/template/AgentBidder.java
+++ vold-landsnes-ex5/auction/src/template/AgentBidder.java
@@ -21,7 +21,8 @@ public class AgentBidder {
 	}
 	
 	
-	public Solution getNewBestPlan(Task task, long time_start, long timeout_bid, List<Task> task_list) {
+	public Solution getNewBestPlan(Task task, long timeout_bid, List<Task> task_list, List<Vehicle> vehicle_list, int tasks_auctioned) {
+		long time_start = System.currentTimeMillis();
 		Long bid = null;
 		
 		// Create a new task list containing the new task
@@ -31,16 +32,16 @@ public class AgentBidder {
 		}
 		possible_task_list.add(task);
 		
-		Solution newSolution = createNewBestPlan(timeout_bid, time_start, possible_task_list);
+		Solution newSolution = createNewBestPlan(timeout_bid, time_start, possible_task_list, vehicle_list, tasks_auctioned);
 		return newSolution;
 	}
 	
 	
-	public Solution createNewBestPlan(long timeout, long time_start, List<Task> task_list) {
-		Solution solution = new Solution(vehicles, task_list);
+	public Solution createNewBestPlan(long timeout, long time_start, List<Task> task_list, List<Vehicle> vehicle_list, int tasks_auctioned) {
+		Solution solution = new Solution(vehicle_list, task_list, tasks_auctioned);
 		
 		Solution best_solution = new Solution(solution);
-		best_solution.build_plan(vehicles, task_list);
+		best_solution.build_plan(vehicle_list, task_list, tasks_auctioned);
 		
 		// Get the best solution for every run of the SLS algorithm
 		double counter = 0;
@@ -48,15 +49,15 @@ public class AgentBidder {
 		for (int i = 0; i < 999999999; i++) {
 			// System.out.println("Iteration " + (i + 1));
 
-			solution = stochastic_local_search(solution, vehicles, task_list, counter);
+			solution = stochastic_local_search(solution, vehicle_list, task_list, counter, tasks_auctioned);
 			
 			if (best_solution.get_cost_of_solution() > solution.get_cost_of_solution()) {
 				best_solution = new Solution(solution);
-				best_solution.build_plan(vehicles, task_list);
+				best_solution.build_plan(vehicle_list, task_list, tasks_auctioned);
 			}
 
 			counter += 1;
-			if (System.currentTimeMillis() - time_start > timeout-1000) {
+			if (System.currentTimeMillis() - time_start > timeout) {
 				break;
 			}
 		}
@@ -64,7 +65,7 @@ public class AgentBidder {
 		long duration = System.currentTimeMillis() - time_start;
 		System.out.println("The plan was generated in " + duration + " milliseconds.");
 		System.out.println("The best plan has a cost of: " + best_solution.get_cost_of_solution() +  "! and is:");
-		for (Vehicle v : vehicles) {
+		for (Vehicle v : vehicle_list) {
 			System.out.println(best_solution.get_action_task_list().get(v));
 		}
 		
@@ -72,9 +73,10 @@ public class AgentBidder {
 	}
 	
 	// Stochastic local search from the book
-	public Solution stochastic_local_search(Solution solution, List<Vehicle> v_list, List<Task> task_list, double temp) {
+	public Solution stochastic_local_search(Solution solution, List<Vehicle> v_list, List<Task> task_list, double temp, int tasks_auctioned) {
 		Solution old_s = solution;
-		List<Solution> neighbours = cn.getNeighbours(old_s, v_list, task_list, temp);
+		old_s.get_action_task_list();
+		List<Solution> neighbours = cn.getNeighbours(old_s, v_list, task_list, temp, tasks_auctioned);
 		Solution best_solution_from_neighbours = lc.getBestSolution(neighbours, old_s, temp);
 		return best_solution_from_neighbours;
 	}
